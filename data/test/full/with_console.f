d7e1f451cca04c9dfe4e435fadfefeb1d7e98de9
frank <dattalo.2@osu.edu>
2019-11-03 20:51:11 -0500

    more testing

--- data/test/full/with_console.f
+++ data/test/full/with_console.f
@@ -16,15 +16,22 @@ var printPath = function(println) {
   return;
 };
 
-var evokeFn = function(println, f, arg1, arg2, arg3) {
-  var _ = println(append("Evoking function: ", f));
+var evokeFn = function(f, arg1, arg2, arg3) {
+  return f(arg1, arg2, arg3);
 };
+
+var add3 = function(a1, a2, a3) { return add(a1, add(a2, a3)); };
+
+_ = println("evokeFn(add3, int(read()), int(read()), int(read())):");
+_ = println(evokeFn(add3, int(read()), int(read()), int(read())));
 
 while (lessOrEqual(i, 10)) {
   _ = println(append(append("Execution ", i), " of 10"));
   _ = print("Enter your name: ");
 
   var name = read();
+
+  _ = println(append("Your name is: ", name));
 
   if (equal(name, "quit")) {
     _ = println("Name was quit!");
